2b73a3a5c7e99046171def28ab36cc01fe549cf4
jackson99011 <jackson99011@gmail.com>
2022-07-31 10:16:36 +0800

    任務 01:建立並設定路由機制 建立post路徑

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { LayoutComponent } from './layout/layout.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'posts',
+    path: '',
+    component: LayoutComponent,
     loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule)
   }
 ];
--- src/app/posts/posts-routing.module.ts
+++ src/app/posts/posts-routing.module.ts
@@ -1,8 +1,28 @@
+import { CreateComponent } from './create/create.component';
+import { PostComponent } from './post/post.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostsComponent } from './posts.component';
 
-const routes: Routes = [{ path: '', component: PostsComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  },
+  {
+    path: 'post/:id',
+    component: PostComponent
+  },
+  {
+    path: 'create',
+    component: CreateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
