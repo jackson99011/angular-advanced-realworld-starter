956cea40eda8a252fba90836d537eac5a9240bad
jackson99011 <jackson99011@gmail.com>
2022-07-31 16:16:20 +0800

    任務 09：練習使用HttpInterceptor 輸入http要求中同步帶入token

--- src/app/posts/create/create.component.ts
+++ src/app/posts/create/create.component.ts
@@ -1,3 +1,7 @@
+import { Article } from './../../interfaces/article';
+import { CreateArticle } from './../../interfaces/create-article';
+import { Router } from '@angular/router';
+import { PostService } from './../../post.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormArray, Validators } from '@angular/forms';
 
@@ -17,7 +21,9 @@ export class CreateComponent implements OnInit {
       this.formBuilder.control('CSS'),
     ])
   });
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+    private postService:PostService,
+    private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -32,6 +38,17 @@ export class CreateComponent implements OnInit {
 
   createPost():void{
     console.log(this.post.value);
+    const createArticle:CreateArticle = {
+      title: this.post.value.title || '' ,
+      description: this.post.value.description || '',
+      body: this.post.value.body || '',
+      tagList: <Array<string>>(this.post.value.tags || []).filter(tag => !!tag)
+    };
+
+    this.postService.createArticle(createArticle).subscribe(() => {
+      this.router.navigate(['/']);
+    });
+
   }
 
 }
