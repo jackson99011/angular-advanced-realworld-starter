2e494a39eb035843bde9b9354a9b35e31d3fa9e8
jackson99011 <jackson99011@gmail.com>
2022-07-31 14:37:59 +0800

    任務 06:Reactive Forms 繫結表單資料

--- src/app/posts/create/create.component.ts
+++ src/app/posts/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
-
-  constructor() { }
+  post = this.formBuilder.group({
+    title: this.formBuilder.control(''),
+    description: this.formBuilder.control(''),
+    body: this.formBuilder.control(''),
+    tags: this.formBuilder.array([
+      this.formBuilder.control('Angular'),
+      this.formBuilder.control('HTML'),
+      this.formBuilder.control('CSS'),
+    ])
+  });
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  addTags(tsg:string):void{
+    this.post.controls.tags.push(this.formBuilder.control(tsg));
+  }
+
+  removeTags(index:number):void{
+    this.post.controls.tags.removeAt(index);
+  }
+
+  createPost():void{
+    console.log(this.post.value);
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
+//登入帳號：demo@miniasp.com
+//登入密碼：123456
 export const environment = {
   apiUrl: 'http://localhost:3000',
   production: false
